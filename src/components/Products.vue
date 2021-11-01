d7f630e6097c05d06e73478205b30fcddf83c1c2
angelicacsti <angelicacastiblanco.ac@gmail.com>
2021-11-01 01:36:23 -0500

    .

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -45,7 +45,6 @@
     <table class="ui teal table">
       <thead>
         <tr>
-          <th></th>
           <th>#</th>
           <th>Código</th>
           <th>Categoría</th>
@@ -56,19 +55,19 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="product in products">
+        <tr>
           <td class="collapsing">
             <div class="ui radio checkbox">
               <input type="radio" /> <label></label>
             </div>
           </td>
-          <td>{{ product.id }}</td>
-          <td>{{ product.codigoproducto }}</td>
-          <td>{{ product.categoriaproducto }}</td>
-          <td>{{ product.descripcion }}</td>
-          <td>{{ product.genero }}</td>
-          <td>{{ product.precio }}</td>
-          <td>{{ product.unidadesdisponibles }}</td>
+          <td>1</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
         </tr>
       </tbody>
     </table>
@@ -82,68 +81,76 @@
 </template>
 
 <script>
-// import jwt_decode from "jwt-decode";
-// import axios from "axios";
-// export default {
-//   name: "Products",
+import jwt_decode from "jwt-decode";
+import axios from "axios";
+export default {
+  name: "Products",
 
-//   data: function() {
-//     return {
-//       products: [],
-//     };
-//   },
+  data: function() {
+    return {
+      id: 0,
+      codigoproducto: "",
+      categoriaproducto: "",
+      descripcion: "",
+      genero: "",
+      precio: "",
+      unidadesdisponibles: 0,
+    };
+  },
 
-//   methods: {
-//     getData: async function() {
-//       if (
-//         localStorage.getItem("token_access") === null ||
-//         localStorage.getItem("token_refresh") === null
-//       ) {
-//         this.$emit("logOut");
-//         return;
-//       }
+  methods: {
+    getData: async function() {
+      if (
+        localStorage.getItem("token_access") === null ||
+        localStorage.getItem("token_refresh") === null
+      ) {
+        this.$emit("completedProducts");
+        return;
+      }
 
-//       await this.verifyToken();
+      await this.verifyToken();
 
-//       let token = localStorage.getItem("token_access");
-//       let userId = jwt_decode(token).user_id.toString();
+      let token = localStorage.getItem("token_access");
+      let userId = jwt_decode(token).user_id.toString();
 
-//       axios
-//         .get(`http://127.0.0.1:8000/user/${userId}/`, {
-//           headers: { Authorization: `Bearer ${token}` },
-//         })
-//         .then((result) => {
-//           this.id = result.data.id;
-//           this.codigoproducto = result.data.codigoproducto;
-//           this.categoriaproducto = result.data.categoriaproducto;
-//           this.descripcion = result.data.descripcion;
-//           this.genero = result.data.genero;
-//           this.precio = result.data.precio;
-//         })
-//         .catch(() => {
-//           this.$emit("logOut");
-//         });
-//     },
+      axios
+        .get(
+          `https://proyecto-games-grupo5-p25.herokuapp.com/user/${userId}/`,
+          { headers: { Authorization: `Bearer ${token}` } }
+        )
+        .then((result) => {
+          this.id = result.data.id;
+          this.category = result.data.category;
+          this.description = result.data.description;
+          this.gender = result.data.gender;
+          this.price = result.data.price;
+          this.available = result.data.available;
+          this.loaded = true;
+        })
+        .catch(() => {
+          this.$emit("completedProducts");
+        });
+    },
 
-//     verifyToken: function() {
-//       return axios
-//         .post(
-//           "http://127.0.0.1:8000/refresh/",
-//           { refresh: localStorage.getItem("token_refresh") },
-//           { headers: {} }
-//         )
-//         .then((result) => {
-//           localStorage.setItem("token_access", result.data.access);
-//         })
-//         .catch(() => {
-//           this.$emit();
-//         });
-//     },
-//   },
-//   created: async function() {
-//     this.getData();
-//   },
-// };
+    verifyToken: function() {
+      return axios
+        .post(
+          "https://proyecto-games-grupo5-p25.herokuapp.com/refresh/",
+          { refresh: localStorage.getItem("token_refresh") },
+          { headers: {} }
+        )
+        .then((result) => {
+          localStorage.setItem("token_access", result.data.access);
+        })
+        .catch(() => {
+          this.$emit("completedProducts");
+        });
+    },
+  },
+  created: async function() {
+    this.getData();
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -179,7 +186,7 @@ body {
   margin: 70px;
   margin-top: 20px;
   margin-left: 330px;
-  margin-bottom: 23%;
+  margin-bottom: 20%;
 }
 .footer {
   position: absolute;
